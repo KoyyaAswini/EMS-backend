e077a3f63f0edc729512759b1e83f4f9f4cebc25
KoyyaAswini <111842097+KoyyaAswini@users.noreply.github.com>
2024-09-01 15:38:45 +0530

    Update Employeedto.java
    removed extra redundant constructors

--- src/main/java/com/EMS/ems/backend/dto/Employeedto.java
+++ src/main/java/com/EMS/ems/backend/dto/Employeedto.java
@@ -9,34 +9,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Employeedto {
-	public Employeedto(Long id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
 
-  private Long id;
-  private String firstName;
-  private String lastName;
-  private String email;
-  
-  public Long getId() {
-	    return id;
-	}
-
-	public String getFirstName() {
-	    return firstName;
-	}
-
-	public String getLastName() {
-	    return lastName;
-	}
-
-	public String getEmail() {
-	    return email;
-	}
-
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
