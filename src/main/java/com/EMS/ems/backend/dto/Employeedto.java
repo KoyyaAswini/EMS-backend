e6b87c71bb578841d19d9c6797619e1c48b1ea87
KoyyaAswini <111842097+KoyyaAswini@users.noreply.github.com>
2024-09-03 13:57:49 +0530

    Update Employeedto.java

--- src/main/java/com/EMS/ems/backend/dto/Employeedto.java
+++ src/main/java/com/EMS/ems/backend/dto/Employeedto.java
@@ -1,36 +1,19 @@
 package com.EMS.ems.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
-
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employeedto {
-	public Employeedto(Long id, String firstName, String lastName, String email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
 
-  private Long id;
-  private String firstName;
-  private String lastName;
-  private String email;
-  
-  public Long getId() {
-	    return id;
-	}
-
-	public String getFirstName() {
-	    return firstName;
-	}
-
-	public String getLastName() {
-	    return lastName;
-	}
-
-	public String getEmail() {
-	    return email;
-	}
-
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
+
